383038fcda82b08179d8dfed703e9528466c36e3
Martuking <martinsch1@gmail.com>
2017-09-30 20:02:26 -0300

    cambie el nombre de main.css a index.css

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var router = express.Router()
 //Aquí se agregan las rutas para los controladores
 router.get('/', function(req, res) {
-  res.sendFile('index.html')
+  res.sendFile(process.cwd()+'/views/index.html')
 })
 
 //Con esto se exportan todas las rutas
--- server.js
+++ server.js
@@ -7,12 +7,12 @@ var mongoose = require('mongoose');
 app.use(bodyParser.json());
 app.use(cors());
 
-app.engine('jade', require('jade').__express)
-app.set('view engine', 'jade')
+//app.engine('jade', require('jade').__express)
+//app.set('view engine', 'jade')
 
 app.use(express.static(__dirname + '/public'))
 app.use(require('./controllers'))
-app.use(require('./views'))
+//app.use(require('./views'))
 
 
 app.listen(3000, function () {
