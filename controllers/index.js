1b86558766f82573fb048c72880c0f028deef6db
Martuking <martinsch1@gmail.com>
2017-09-30 19:43:58 -0300

    se agrega el archivo index.js y se trabaja en server.js

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+var express = require('express')
+var router = express.Router()
+//Aquí se agregan las rutas para los controladores
+router.get('/', function(req, res) {
+  res.sendFile('index.html')
+})
+
+//Con esto se exportan todas las rutas
+module.exports = router
--- server.js
+++ server.js
@@ -7,6 +7,14 @@ var mongoose = require('mongoose');
 app.use(bodyParser.json());
 app.use(cors());
 
+app.engine('jade', require('jade').__express)
+app.set('view engine', 'jade')
+
+app.use(express.static(__dirname + '/public'))
+app.use(require('./controllers'))
+app.use(require('./views'))
+
+
 app.listen(3000, function () {
 	console.log('Listening on port 3000...');
 });
