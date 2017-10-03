3a322ce80c505d52198caf4023cf77b767092a77
Martuking <martinsch1@gmail.com>
2017-10-03 12:00:25 -0300

    deje la cagada probando cosas volado asiq estoy trantando de arreglarlo

--- controllers/personal.js
+++
@@ -1,9 +0,0 @@
-var express = require('express')
-var router = express.Router()
-//Aquí se agregan las rutas para los controladores
-router.get('/controllers/personal', function(req, res) {
-  res.sendFile(process.cwd()+'/views/personal.html')
-})
-
-//Con esto se exportan todas las rutas
-module.exports = router
