cf451a2de3112e40d039449ebd0c30da97bf4f6f
Martuking <martinsch1@gmail.com>
2017-10-05 12:44:04 -0300

    Agregando una nueva ruta para personal a través de index.js, no esta funcionado ahora

--- controllers/personalController.js
+++ controllers/personalController.js
@@ -1,10 +1,9 @@
 var express = require('express')
 var router = express.Router()
 //Aquí se agregan las rutas para los controladores
-router.get('/', function(req, res) {
-  res.sendFile(process.cwd()+'/views/index.html')
+router.get('/personal', function(req, res) {
+  res.sendFile(process.cwd()+'/views/personal.html')
 })
-router.use('/personal',require('./personalController'))
 
 //Con esto se exportan todas las rutas
 module.exports = router
