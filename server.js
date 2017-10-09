deea6e9937ecc18043bd6b1eec6806df9cc6b0e2
Martuking <martinsch1@gmail.com>
2017-10-09 16:09:45 -0300

    logre hacer funcionar el controlador de personal, renderea ahora personal.html, el siguiente paso es probar los methodos crud, pero para eso tengo q habilitar un db

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ var cors = require('cors');
 var bodyParser = require('body-parser');
 var app = express();
 var mongoose = require('mongoose');
+var morgan = require('morgan');
+var router = express.Router();
 
+//var personal = require('./controllers/personal')
+
+app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(cors());
 
-//app.engine('jade', require('jade').__express)
-//app.set('view engine', 'jade')
-
 app.use(express.static(__dirname + '/public'))
 app.use(require('./controllers'))
-//app.use(require('./views'))
-
-
+//app.use(require('./controllers/personal'))
 app.listen(3000, function () {
 	console.log('Listening on port 3000...');
 });
